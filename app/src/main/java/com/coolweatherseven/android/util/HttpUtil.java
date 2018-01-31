ac917c663326adc4650b784612690f08d9eec952
suncuigai <hudie0011@163.com>
2018-01-31 22:40:10 +0800

    完成遍历省市县三级列表的功能能

---
+++ app/src/main/java/com/coolweatherseven/android/util/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.coolweatherseven.android.util;
+
+import android.app.DownloadManager;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2018/1/31.
+ */
+
+public class HttpUtil {
+
+    public  static void  sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+       Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
