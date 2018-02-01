116da540dd6528fcf6a46505510d0ce301b506f6
suncuigai <hudie0011@163.com>
2018-02-01 21:29:54 +0800

    增加后台自动更新天气的功能和修改程序图标和名称

--- app/src/main/java/com/coolweatherseven/android/WeatherActivity.java
+++ app/src/main/java/com/coolweatherseven/android/WeatherActivity.java
@@ -22,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweatherseven.android.gson.Forecast;
 import com.coolweatherseven.android.gson.Weather;
+import com.coolweatherseven.android.service.AutoUpdateService;
 import com.coolweatherseven.android.util.HttpUtil;
 import com.coolweatherseven.android.util.Utility;
 
@@ -176,7 +177,7 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
-       // loadBingPic();
+        loadBingPic();
     }
 
 
@@ -216,8 +217,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
-//        Intent intent = new Intent(this, AutoUpdateService.class);
-//        startService(intent);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+       startService(intent);
 
 
     }
